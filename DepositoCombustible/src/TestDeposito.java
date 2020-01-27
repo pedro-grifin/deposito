5f36a725b7ef3eeb89d9bdb3ae3e45a4e8c57302
Pedro Guzman <pedro.grifin@gmail.com>
2020-01-27 12:12:36 +0100

    Inicial

---
+++ DepositoCombustible/src/TestDeposito.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDeposito {
+
+	@Test
+	public void testDepositoCombustible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoNivel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
